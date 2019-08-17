753a06e6a0e15289ed966d56124bb958a44228e6
acarmonabelmonte <acarmonabelmonte@gmail.com>
2019-08-17 15:52:10 +0200

    mejoras en el script y web service

--- app.py
+++ app.py
@@ -3,24 +3,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index(city):
-    # city = 'Barcelona'
+@app.route('/weather-forecast/<city>')
+def getInfo(city):
+    # api-endpoint 
     api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
+
+    # key para conectar con la API de Openweathermap
     apikey = "953886b865382df790a3cda9564a301e"
+
+    # url para obtener la información sobre una ciudad concreta
     url = api_endpoint + "?q=" + city + "&appid=" + apikey
     
-    r = requests.get(url.format(city)).json()
-    
-    weather = {
-        'city' : city,
-        'temperature' : round(r['main']['temp'] - 273.15, 2)
-    }
-    
-    result = 'Weather forecast for tomorrow in ' + city.capitalize() + ' Temperature: ' + str(weather['temperature']) + ' ºC'
-    
-    print (result)
-    return result
+    # realizar solicitud GET sobre la url que le pasemos
+    r = requests.get(url)
+
+    # devolver resultados en formato JSON
+    return r.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -1,18 +1,33 @@
-from app import index
-import os
+import requests
 
-os.system('FLASK_APP=app.py FLASK_DEBUG=1 python -m flask run')
-
-print("======= Ciudades disponibles =======")
+# array con ciudades de España de ejemplo
+print('========== Ciudades disponibles ==========')
 cities = ["Barcelona", "Madrid", "Valencia", "Murcia", "Sevilla", "Galicia"]
 for x in cities:
-  print('- ' +  x)
+      print('- ' +  x)
 
+# seleccionar la ciudad
 print("¿De qué ciudad desea saber el tiempo?")
 city = input()
 
+# realizamos la petición GET si la ciudad se encuentra entre las opciones de ejemplo
 if city.capitalize() in cities:
-    index(city)
+    # api-endpoint 
+    api_endpoint = "http://127.0.0.1:5000/weather-forecast/"
+
+    # url
+    url = api_endpoint + city
+
+    # realizar solicitud GET sobre la url pasada
+    r = (requests.get(url)).json()
+
+    weather = {
+        'city' : city,
+        'temperature' : round(r['main']['temp'] - 273.15, 2)
+    }
+
+    result = 'Weather forecast for tomorrow in ' + city.capitalize() + ' Temperature: ' + str(weather['temperature']) + ' ºC'
+
+    print(result)
 else:
     print('La ciudad que ha introducido no existe entre las opciones')
-
